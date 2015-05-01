42d10a527f9d0d8a3ae0daf4f7dd014c4fae2678
micky24 <micky.teguh@gmail.com>
2015-05-01 12:04:08 +0700

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -8,9 +8,9 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/benjamin74/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/micky24/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/benjamin74/3proxy/master/3proxyinit
-chmod  +x /etc/init.d/3proxyinit
-update-rc.d 3proxyinit defaults
+wget --no-check-certificate https://raw.github.com/micky24/3proxy/master/3proxy
+chmod  +x /etc/init.d/3proxy
+update-rc.d 3proxy defaults
